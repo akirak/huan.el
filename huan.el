08930e34047bec5af027c116fc474b69bc943ee6
Akira Komamura <akira.komamura@gmail.com>
2022-02-27 21:08:29 +0900

    Initial commit

---
+++ huan.el
@@ -0,0 +1,55 @@
+;;; huan.el --- Replace things with predefined alternatives -*- lexical-binding: t -*-
+
+(require 'cl-lib)
+(require 'thingatpt)
+(require 'subr-x)
+
+(defcustom huan-mode-symbols-alist
+  '((emacs-lisp-mode
+     ("defun" "cl-defun")
+     ("defmacro" "cl-defmacro")
+     ("cl-defeneric" "cl-defmethod")
+     ("let" "let*")
+     ("eq" "eql" "equal")
+     ("memq" "memql" "member")
+     ("defvar" "defconst" "defcustom")
+     ("cl-flet" "cl-flet*" "cl-labels")))
+  "FIXME"
+  :type '(alist :key-type (symbol :tag "Major mode")
+                :value-type (repeat (repeat string))))
+
+;;;###autoload
+(defun huan-defun ()
+  "Change the type of the defun at point."
+  (interactive)
+  (save-excursion
+    (beginning-of-defun-raw)
+    (when (re-search-forward "\\b" nil t)
+      (huan-alternate-symbol))))
+
+;;;###autoload
+(defun huan-symbol ()
+  "Change the symbol at point."
+  (interactive)
+  (when-let (entries (cdr (assq major-mode huan-mode-symbols-alist)))
+    (pcase (bounds-of-thing-at-point 'symbol)
+      (`(,start . ,end)
+       (let ((cur (buffer-substring-no-properties start end)))
+         (when-let (entry (cl-find-if (lambda (list) (member cur list))
+                                      entries))
+           (let ((new (huan--maybe-complete-candidate cur entry)))
+             (save-excursion
+               (delete-region start end)
+               (goto-char start)
+               (insert new)))))))))
+
+(defun huan--maybe-complete-candidate (cur candidates)
+  "Pick an alternative to CUR from CANDIDATES."
+  (let ((candidates (cl-remove cur candidates :test #'equal)))
+    (if (= 1 (length candidates))
+        (car candidates)
+      (completing-read (format "Replace %s with: " cur)
+                       candidates))))
+
+(provide 'huan)
+;;; huan.el ends here
